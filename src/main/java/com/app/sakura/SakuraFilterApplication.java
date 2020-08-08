e01ca45858e692c8998b1fd014a2da6087f53d2a
ZainKhowaja <you@example.com>
2020-08-08 21:59:18 +0500

    Initial Commit

---
+++ src/main/java/com/app/sakura/SakuraFilterApplication.java
@@ -0,0 +1,13 @@
+package com.app.sakura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakuraFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakuraFilterApplication.class, args);
+	}
+
+}

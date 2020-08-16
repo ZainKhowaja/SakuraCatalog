572db223e4edb4e7211a7722fe9cca3f7e222e15
ZainKhowaja <you@example.com>
2020-08-16 13:37:27 +0500

    Code Clean up

--- src/main/java/com/app/sakura/controller/MainController.java
+++ src/main/java/com/app/sakura/controller/MainController.java
@@ -1,36 +1,20 @@
 package com.app.sakura.controller;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Controller;
 
 import com.app.sakura.enums.SakuraScreen;
 import com.app.sakura.util.ScreenUtils;
 
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
 import javafx.scene.control.Tab;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
 
 @Controller
 public class MainController {
@@ -51,11 +35,6 @@ public class MainController {
 	
 	@FXML
 	private Tab addProductTab;
-	
-	@Autowired
-	private ConfigurableApplicationContext context;
-
-	private FileChooser fileChooser = new FileChooser();
 
 	@FXML
 	void exit(MouseEvent event) {
@@ -83,7 +62,6 @@ public class MainController {
 	
 	public void switchScreen(Event event) throws IOException {
 		try {
-			System.out.println(event.getSource().equals(addProductTab) +","+event.getSource().equals(productSearchTab));
 			if(event.getSource().equals(addProductTab)) {
 				addProductTab.setContent(screen.getFXMLNode(SakuraScreen.ADD_PRODUCT));
 			}else if(event.getSource().equals(productSearchTab)) {
